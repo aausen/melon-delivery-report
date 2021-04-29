d0606937973858cc52a577ab4e8bf9da9e1025aa
Annie Ausen <ausen.annie@gmail.com>
2021-04-29 01:58:30 +0000

    delivery-report-function

--- produce-summary.py
+++ produce-summary.py
@@ -1,43 +1,34 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def melon_delivery(day, path):
+"""Given day number and path to deliveries, produce report
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+Opens the deliveries file at [path], processes each line, and generates a report in all uppercase"""
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print("Day", day)
+    # prints the day
+    delivery_log = open(path)
+    #opens the file
+
+    for line in delivery_log:
+        # to loop through each line in the document
+        line = line.rstrip()
+        # removes the whitespace
+        words = line.split('|')
+        # breaks up the words in the input so they can be looked at individually
+
+        melon = words[0]
+        # says where to find the melon
+        count = words[-2]
+        # says where to find the count
+        amount = words[-1]
+        # says where to find the amount
+
+        print("Delivered {} {}s for total of ${}".format(
+         count, melon, amount))
+        # prints out the report that needs to be read
+    delivery_log.close()
+    # closing the file
 
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+melon_delivery(1, "um-deliveries-20140519.txt")
+melon_delivery(2, "um-deliveries-20140520.txt")
+melon_delivery(3, "um-deliveries-20140521.txt")
